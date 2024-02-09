4f949f25eb8afc84a0857ad6e30e74fd84904002
Colby McClure <colbymc0801@gmail.com>
2024-02-09 10:57:13 -0600

    Updated mcclureLab2.s
    - Added some input validation
    - Added the base for the subroutines
    - Started on the choice label and what that would entail

--- mcclureLab2.s
+++ mcclureLab2.s
@@ -43,7 +43,7 @@ optionCheck:
     beq readError @ Branch to readError if equal 
 
     ldr r1, =input @ Load r1 with the address of the input  
-    ldr r3, [r1] @ Load r1 with the contents of the input 
+    ldr r3, [r1] @ Load r3 with the contents of the input 
     cmp r3, #4 @ Compare r1 with 4
     bgt beginPrompt @ Loop back to the beginning if r1 is greater than 4
     cmp r3, #1 @ Compare r1 with 1
@@ -57,8 +57,63 @@ operandCheck:
 
     ldr r0, =numPrompt @ Load the operand instruction message 
     bl printf @ Print the message 
-    b exit @ TODO: MOVE THIS 
+    
+    ldr r0, =numInputPattern @ Load the integer input pattern 
+    ldr r1, =input @ Load the input variable 
+    bl scanf @ Scan for input 
 
+    ldr r1, =input @ Load r1 with the address of the input 
+    ldr r4, [r1] @ Load r4 with the contents of the input 
+
+    cmp r4, #0 @ Compare the contents of r4 with 1
+    blt operandCheck @ If r4 is less than 1, then loop back to operandCheck 
+
+    ldr r0, =numInputPattern @ Load the integer input pattern 
+    ldr r1, =input @ Load the input variable 
+    bl scanf @ Scan for input 
+
+    ldr r1, =input @ Load r1 with the address of the input 
+    ldr r5, [r1] @ Load r5 with the contents of the input 
+
+    cmp r5, #0 @ Compare the contents of r5 with 1
+    blt operandCheck @ If r5 is less than 1, then loop back to operandCheck
+
+    @ TODO: Go back and check for input validation when checking for 0 as the dividend 
+
+@ This branch checks and sees what subroutine is needed for what operation based on what the user input 
+@**********
+operationCall:
+@**********
+
+    cmp r3, #1 @ Check to see if the user wanted to add
+    bleq addRoutine @ Branch to subroutine if that's what user wanted 
+
+    cmp r3, #2 @ Check to see if the user wanted to subtract 
+    bleq subRoutine @ Branch to subroutine if that's what user wanted 
+
+    cmp r3, #3 @ Check to see if user wanted to multiply 
+    bleq mulRoutine @ Branch to subroutine if that's what user wanted 
+
+    cmp r3, #4 @ Check to see if user wanted to divide 
+    bleq divRoutine @ Branch to subroutine if that's what user wanted 
+
+    b choice  @ TODO: REMOVE THIS LATER
+
+@**********
+addRoutine:
+@**********
+
+@**********
+subRoutine:
+@**********
+
+@**********
+mulRoutine:
+@**********
+
+@**********
+divRoutine: 
+@**********
 
 @ This label is for incorrect input and it branches back to the prompt 
 @**********
@@ -86,7 +141,7 @@ exit:
 prompt: .asciz "Hello, please enter in the number that corresponds to the operation you would like to use: \n"
 
 .balign 4
-options: .asciz "Add [1] Subtract [2] Multiply [3] Divide [4] \n" 
+options: .asciz "[1] Add [2] Subtract [3] Multiply [4] Divide \n" 
 
 .balign 4 
 numInputPattern: .asciz "%d"
@@ -108,4 +163,13 @@ op2: .word 0
 
 .balign 4
 numPrompt: .asciz "Please enter your two operands one at a time: \n"
+
+.balign 4
+divideError: .asciz "You cannot divide a number by zero, please try again \n" 
+
+.balign 4
+tryAgain: .asciz "Would you like to perform another operation? \n"
+
+.balign 4
+options2: .asciz "[1] Yes [2] No \n"
 
